01ed9a01d35da4e16cf3ba9116cee84861940e8c
Isha <ishapatel2401@gmail.com>
2024-08-02 11:36:03 +0530

    create index.ts

---
+++ app/components/index.ts
@@ -0,0 +1,4 @@
+import SignInFacebook from "./SignInWithFacebook";
+import SignInGoogle from "./SignInWithGoogle";
+import PhoneSignIn from "./SignInWithPhone";
+export { SignInFacebook, SignInGoogle, PhoneSignIn };
