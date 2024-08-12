e02911222bcca1b5ca908799bc2364b4f0655ccd
Isha <ishapatel2401@gmail.com>
2024-08-12 12:07:28 +0530

    add isUserExist function

--- app/components/SignInWithGoogle.tsx
+++ app/components/SignInWithGoogle.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 import { Button } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import firestore from '@react-native-firebase/firestore';
+import { isUserExist } from '../utils/SignInWithOAuth';
 
 export default function SignInGoogle() {
-    const usersCollection = firestore().collection('Users');
     GoogleSignin.configure({
         webClientId:
             '1073107012067-s153pacfd8h98613e5q0fcip8p5ivs89.apps.googleusercontent.com',
@@ -20,22 +19,14 @@ export default function SignInGoogle() {
 
         // Get the users ID token
         try {
-            const { idToken, user } = await GoogleSignin.signIn();
-            console.log('User data success', user);
+            const { idToken } = await GoogleSignin.signIn();
 
             // Create a Google credential with the token
             const googleCredential = auth.GoogleAuthProvider.credential(idToken);
             console.log('google-cred:', JSON.stringify(googleCredential));
 
-            usersCollection.add({
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                imageUrl: user.photo,
-                provider: googleCredential.providerId,
-            });
-            // Sign-in the user with the credential
-            return auth().signInWithCredential(googleCredential);
+            isUserExist(googleCredential);
+
         } catch (err) {
             console.log('idToken error:', JSON.stringify(err, null, 2));
         }
