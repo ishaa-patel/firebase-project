df25b848fef6b6adbaffe0c805fbcfc413e5dc0f
Isha <ishapatel2401@gmail.com>
2024-08-12 12:05:53 +0530

    update

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,5 @@
-import SignInFacebook from "./SignInWithFacebook";
-import SignInGoogle from "./SignInWithGoogle";
-import PhoneSignIn from "./SignInWithPhone";
-import EmailSignIn from "./SignInWithEmail";
-export { SignInFacebook, SignInGoogle, PhoneSignIn, EmailSignIn };
+import SignInGoogle from './SignInWithGoogle';
+import PhoneSignIn from './SignInWithPhone';
+import EmailSignIn from './SignInWithEmail';
+import Facebook from './Facebook';
+export { SignInGoogle, PhoneSignIn, EmailSignIn, Facebook };
